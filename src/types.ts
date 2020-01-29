88ed389de805b1271e60972b73df18e213c369c0
midori <cleoold@gmail.com>
2020-01-28 23:24:04 -0500

    ticker homepage

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,22 @@
 /** stylesheet for individual elements */
 export type Style = {};
 
-export type ITimerProp = { style?: Style };
-export type ITimerState = { now: Date };
-export type IWeatherProp = { style?: Style, city: string };
-export type IWeatherState = { success: boolean, data: any };
+export type IAppProp = {};
+export type IAppState = {};
+
+export type ITimerProp = {
+  style?: Style
+};
+export type ITimerState = {
+  now: Date
+};
+
+export type IWeatherProp = {
+  city: string,
+  style?: Style
+};
+export type IWeatherState = {
+  cityName: string,
+  success: boolean,
+  data: any
+};
